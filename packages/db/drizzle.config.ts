a27b5205f3cf878ac8e9b626883e9114e3b96377
tsmsumu <tsmsumu@users.noreply.github.com>
2026-04-21 01:02:31 +0700

    fix(db): Drizzle Config must respect DATABASE_URL so it pushes to the exact same file NextJS is connecting to!

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {
-    url: 'file:local.db',
+    url: process.env.DATABASE_URL || 'file:local.db',
   },
 });
